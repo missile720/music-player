c3a89c79291e9a1869e21a32e1201f98b4316181
Misael <misaelbolainez@gmail.com>
2023-06-01 13:40:47 -0700

    feat(layout): Removed default Vite stuff and setup component folder with login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Login.css"
+
+function Login() {
+  return (
+    <div className='container-fluid d-flex align-items-center justify-content-center h-100'>
+        <div className='login'>
+            Login
+        </div>
+    </div>
+  )
+}
+
+export default Login
