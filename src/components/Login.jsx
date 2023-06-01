8d9a628e55eed88ac577aaaeef82b5083fdd0d32
Misael Bolainez <misaelbolainez@gmail.com>
2023-06-01 15:59:38 -0700

    feat(starting login): Setup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import "./Login.css"
 
 function Login() {
   return (
     <div className='container-fluid d-flex align-items-center justify-content-center h-100'>
-        <div className='login'>
-            Login
+        <div className='login d-flex align-items-center flex-column'>
+            <h1>Login</h1>
+            <p className="d-flex align-self-start">Email</p>
+            <input type="email" />
         </div>
     </div>
   )
