27b1a2c5ef9dcfdaf72cce2ebd2e49e1093f7595
unknown <ivan.piceno171@gmail.com>
2023-06-01 15:48:26 -0700

    feat(current song basic layout)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Login from "./components/Login"
+import CurrentSong from "./components/CurrentSong"
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Login/>
+      <CurrentSong></CurrentSong>
     </>
   )
 }
