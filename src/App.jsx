8885a800206f53646604fba1357690386fc1729e
Dawid Fourie <dawief4@gmail.com>
2023-06-01 15:50:30 -0700

    feat(layout/Nav-component) finished innitial layout and Nav bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Login from "./components/Login"
+import Main from "./components/Main-window"
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Login/>
+      {Main ? <Main /> : <Login/>}
+      
     </>
   )
 }
