20abb067efc747f71af1e8c52090e6e1df5995be
Dawid Fourie <dawief4@gmail.com>
2023-06-01 13:53:12 -0700

    feat(layout) Added columns

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,29 @@
-
+import './main.css'
 
 function Main() {
     return (
-    <div>
-        <h1>Hello</h1>
+    <div className='container-fluid h-100'>
+        <div className='row h-100'>
+            {/* left column */}
+            <div className='col-6'>
+                {/* Nav/search bar */}
+                <div className='col-12'>
+                    Nav/search bar
+                </div>
+                <div className='col-12'>
+                    <h3>Library</h3>
+                </div>
+                {/* Library playlist */}
+                <div className='col-12'>
+
+                </div>
+            </div>
+
+            {/* right column */}
+            <div className='col-6'>
+                column
+            </div>
+        </div>
     </div>
     )
 }
