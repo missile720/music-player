93481aa9b2849d841f4537a91862e314dbbac4a2
Misael Bolainez <misaelbolainez@gmail.com>
2023-06-02 11:10:05 -0700

    feat(login/sign up): Created base logic and layout of the login/sign up page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,67 @@
+import {useState} from "react"
 import "./Login.css"
 
 function Login() {
+  const [loginPage, setLoginPage] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  function handleChange(event){
+    const {name, value} = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: value
+    }))
+  }
+
+  function handleLogin(event){
+    event.preventDefault();
+    console.log(formData)
+  }
+
+  function handleSignUp(event){
+    event.preventDefault();
+    console.log(formData)
+  }
+
+  function signUpPage(){
+    setLoginPage(true);
+  }
+
   return (
     <div className='container-fluid d-flex align-items-center justify-content-center h-100'>
         <div className='login d-flex align-items-center flex-column'>
-            <h1>Login</h1>
-            <p className="d-flex align-self-start">Email</p>
-            <input type="email" />
+            <h1 className="login-header">{loginPage ? "Sign Up" : "Login"}</h1>
+            <form className="d-flex flex-column login-form" onSubmit={loginPage ? handleLogin : handleSignUp}>
+              <input 
+                type="email"
+                name="email" 
+                placeholder="Email address"
+                onChange={handleChange}
+                value={formData.email}
+              />
+              <input 
+                type="password"
+                name="password" 
+                placeholder="Password"
+                onChange={handleChange}
+                value={formData.password}
+              />
+              {loginPage && 
+                <input 
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm password"
+                  onChange={handleChange}
+                  value={formData.confirmPassword}
+                />
+              }
+              <button className="login-button">{loginPage ? "Sign Up" : "Sign in"}</button>
+              {!loginPage && <p className="align-self-center signup-text" onClick={signUpPage}>Sign up</p>}
+            </form>
         </div>
     </div>
   )
