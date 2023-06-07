bcdfb5e5460c63bc51e974ea46ee3f34f87b2a4b
Ernest0G <ernesto7gutierrez@gmail.com>
2023-06-07 14:24:52 -0700

    feat(login): Gets access token from Amazon through Login With Amazon (LWA)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,26 @@
-import {useContext} from 'react';
-import {Context} from "../Context"
-import "./Login.css"
+import { useContext } from "react";
+import { Context } from "../Context";
+import "./Login.css";
 
 function Login() {
-  const {loginSpotify} = useContext(Context);
+  const { loginSpotify } = useContext(Context);
 
   return (
-    <div className='container-fluid d-flex align-items-center justify-content-center h-100'>
-        <div className='login d-flex align-items-center flex-column'>
-            <h1 className="login-header">Login to account:</h1>
-              <button className="login-button" onClick={loginSpotify}>Connect Spotify Account</button>
-              {/* <button className="login-button">Connect SoundCloud Account</button> */}
-              <button className="login-button">Connect ITunes Account</button>
-        </div>
+    <div className="container-fluid d-flex align-items-center justify-content-center h-100">
+      <div className="login d-flex align-items-center flex-column">
+        <h1 className="login-header">Login to account:</h1>
+        <button className="login-button" onClick={loginSpotify}>
+          Connect Spotify Account
+        </button>
+        {/* <button className="login-button">Connect SoundCloud Account</button> */}
+        <button
+          className="login-button"
+          onClick={loginAmazon}
+          id="LoginWithAmazon"
+        ></button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
