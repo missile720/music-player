27b8353648e6ff1ccc4cfb008b5597294100a19b
Traven Gonzales <travengonzales@gmail.com>
2023-06-08 20:40:53 -0700

    feat(display): Cleaned up the console log formatting of the YouTube Context

--- src/YouTubeContext.jsx
+++ src/YouTubeContext.jsx
@@ -3,6 +3,15 @@ import config from "./apiConfig.js"
 
 const YouTubeContext = createContext()
 
+/**
+ * A YouTube Context that allows a user to login to their YouTube and console 
+ * log their playlists.
+ * @param {ReactElement} children The children of the context provider 
+ * @returns {ReactElement} A react element with the following provided as context:
+ * -youTubeAccessToken {string} A string denoting a user's access token
+ * -loginYouTube() {func} An event handler. Used to take the user to an OAuth2 
+ * screen for YouTube
+ */
 function YouTubeContextProvider({ children }) {
     // State Variables
     const [youTubeAccessToken, setYouTubeAccessToken] = useState("")
@@ -77,10 +86,10 @@ function YouTubeContextProvider({ children }) {
     /**
      * Loads the user's playlists. Performs a fetch request then console logs the user's
      * playlists. 
-     * 
      */
     async function loadYouTubePlaylists() {
         try {
+            // Used for API calls to get playlists
             const playlistArgs = new URLSearchParams({
                 "part": [
                     "snippet",
@@ -91,11 +100,9 @@ function YouTubeContextProvider({ children }) {
                 "key": apiKey
             })
 
+            // Used for API calls to get playlistItems (Effectivly the songs of a playlist)
             const playlistItemsArgs = new URLSearchParams({
-                "part": [
-                    "snippet",
-                    "contentDetails"
-                ],
+                "part": "snippet",
                 "access_token": youTubeAccessToken,
                 "key": apiKey
             })
@@ -105,18 +112,47 @@ function YouTubeContextProvider({ children }) {
 
             console.log("--------YouTube Playlists-------")
             for (let i = 0; i < playlistData.items.length; i++) {
-                console.log(`${i}: ${playlistData.items[i].snippet.title}`)
-                playlistItemsArgs.append("id", playlistData.items[i].contentDetails.id)
-                const playlistItemsRes = await fetch(youTubeApiPlaylistItemsUrl + playlistItemsArgs)
-                const playlistItemsData = await playlistItemsRes.json()
-                console.log(playlistItemsData)
-
-                playlistItemsArgs.delete("id")
+                await logPlaylist(playlistData.items[i], i, playlistItemsArgs)
             }
 
         } catch (error) {
             console.log(error)
         }
+    }
+
+    /**
+     * Logs a playlist and its contents to the console.
+     * @param {Object} item An object representing an entry in the items
+     * member of the playlist object from YouTube's Data API
+     * @param {Number} i The index of the playlist in the user's playlists
+     * @param {URLSearchParams} playlistItemsArgs The search params
+     * for the fetch request of a playlist's playlistItems. Is missing
+     * the playlistId param as that is appended and deleted in 
+     * this call
+     */
+    async function logPlaylist(item, i, playlistItemsArgs) {
+        console.log(`${i}: ${item.snippet.title}`)
+        playlistItemsArgs.append("playlistId", item.id)
+
+        const playlistItemsRes = await fetch(youTubeApiPlaylistItemsUrl + playlistItemsArgs)
+        const playlistItemsData = await playlistItemsRes.json()
+
+        const { items } = playlistItemsData
+        for (let playlistItem of items) {
+            logPlaylistItem(playlistItem)
+        }
+
+        playlistItemsArgs.delete("playlistId")
+    }
+
+    /**
+     * Logs the title of a song in a YouTube playlist to the console, 
+     * formatted to be displayed under a playlist name.
+     * @param {Object} playlistItem A playlistItem object as denoted
+     * in YouTube API's playlistItem documentation.
+     */
+    function logPlaylistItem(playlistItem) {
+        console.log(`\t-${playlistItem.snippet.title}`)
     }
 
     return (
