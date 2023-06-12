f9af9e069cdfabb93b96c122091546b2312a7d1a
Traven Gonzales <travengonzales@gmail.com>
2023-06-12 15:22:47 -0700

    chore(restructure): Restuctured Nav component to separate svg files to assets folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { Context } from "../Context"
 import defaultPfp from "../assets/defaultProfilePic.svg"
+import searchIcon from "../assets/searchIcon.svg"
 
 function Nav() {
     const { userProfileSpotify } = useContext(Context);
@@ -24,7 +25,7 @@ function Nav() {
         <div className="row h-100 align-items-center">
             {/* user icon */}
             <div className="col-2">
-                <img src={getProfilePic()} width="80" height="80" alt="" />
+                <img src={getProfilePic()} width="80" height="80" alt="User's profile picture" />
             </div>
 
             <div className="col-9 align-self-center">
@@ -32,9 +33,7 @@ function Nav() {
                 <div className="input-group mb-3">
                     <input type="text" className="form-control" placeholder="Search Playlist" aria-label="Search Playlist" aria-describedby="button-addon2" />
                     <button className="btn btn-outline-warning" type="button" id="button-addon2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                        </svg>
+                        <img src={searchIcon} width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16" alt="Search Icon" />
                     </button>
                 </div>
             </div>
