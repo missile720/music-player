bc88612bd29d2fa5641ff68643bd5159828a29c5
Traven Gonzales <travengonzales@gmail.com>
2023-06-15 13:36:59 -0700

    refactor(functionality): Removed usage of YouTubeContext

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,15 @@
 import { useContext } from 'react';
-import { Context } from "../Context"
-import { YouTubeContext } from "../YouTubeContext"
+import { Context } from "../context/Context"
 import "./Login.css"
 
 function Login() {
   const { loginSpotify } = useContext(Context)
-  const { loginYouTube } = useContext(YouTubeContext)
 
   return (
     <div className='container-fluid d-flex align-items-center justify-content-center h-100'>
       <div className='login d-flex align-items-center flex-column'>
         <h1 className="login-header">Login to account:</h1>
         <button className="login-button" onClick={loginSpotify}>Connect Spotify Account</button>
-        {/* <button className="login-button">Connect SoundCloud Account</button> */}
-        <button className="login-button" onClick={loginYouTube}>Connect YouTube Account</button>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ContextProvider } from "./Context"
-import { YouTubeContextProvider } from "./YouTubeContext"
+import { ContextProvider } from "./context/Context"
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <YouTubeContextProvider>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
-    </YouTubeContextProvider>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
 )
