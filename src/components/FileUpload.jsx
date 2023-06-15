47dee3dd48c3d914883dd0e95c93a1e8b646f89f
Ernest0G <ernesto7gutierrez@gmail.com>
2023-06-15 14:56:25 -0700

    feat(local music): Allowed for saving of the new playlist to be saved to local storage

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,107 +1,186 @@
-import { useState } from 'react'
-import songImage from '../assets/song.svg'
-import './FileUpload.css'
+import { useState } from "react";
+import defaultSongImage from "../assets/song.svg";
+import "./FileUpload.css";
 
 const FileUpload = () => {
-    const [playlistData, setPlaylistData] = useState({
-        coverImage: '',
-        playlistName: '',
-        songs: []
+  const [playlistData, setPlaylistData] = useState({
+    coverImage: "",
+    playlistName: "",
+    tracks: [],
+  });
+
+  function handlePLaylistCoverChange(event) {
+    const file = event.target.files[0];
+    const blob = new Blob([file], { type: "image/*" });
+    const imageUrl = URL.createObjectURL(blob);
+
+    setPlaylistData({
+      ...playlistData,
+      coverImage: imageUrl,
     });
+  }
 
-    function handlePLaylistCoverChange(event) {
-        const file = event.target.files[0];
-        const blob = new Blob([file], { type: 'image/*' })
-        const imageUrl = URL.createObjectURL(blob);
+  function handlePlaylistChangeName(event) {
+    setPlaylistData({
+      ...playlistData,
+      playlistName: event.target.value,
+    });
+  }
 
-        setPlaylistData({
-            ...playlistData,
-            coverImage: imageUrl
-        })
-    }
+  function handleFileUpload(event) {
+    const jsMediaTags = window.jsmediatags;
+    const files = [...event.target.files];
 
-    function handlePlaylistChangeName(event) {
-        setPlaylistData({
-            ...playlistData,
-            playlistName: event.target.value
-        })
-    }
+    const songFiles = files.map((song) => {
+      const blob = new Blob([song], { type: "audio/*" });
+      const songUrl = URL.createObjectURL(blob);
 
-    function handleFileUpload(event) {
-        const jsMediaTags = window.jsmediatags;
-        const files = [...event.target.files];
+      // Some of files uploaded may not have all the metadata provided when
+      // retrieved by jsmediatags, so they are given the following default values
+      const songData = {
+        name: song.name,
+        artist: "unknown",
+        songImage: defaultSongImage,
+        audioSource: songUrl,
+      };
 
-        const songFiles = files.map(song => {
-            const blob = new Blob([song], { type: 'audio/*' })
-            const songUrl = URL.createObjectURL(blob);
+      jsMediaTags.read(song, {
+        onSuccess: function (tag) {
+          if (tag.tags.title) {
+            songData.name = tag.tags.title;
+          }
+          if (tag.tags.artist) {
+            songData.artist = tag.tags.artist;
+          }
+          if (tag.tags.picture) {
+            const byteArray = new Uint8Array(tag.tags.picture.data);
+            const blob = new Blob([byteArray], { type: "image/*" });
+            const songPicture = URL.createObjectURL(blob);
 
-            // Some of files uploaded may not have all the metadata provided when 
-            // retrieved by jsmediatags, so they are given the following default values
-            const songData = {
-                name: song.name,
-                artist: 'unknown',
-                songImage: songImage,
-                duration: 'unknown',
-                audioSource: songUrl
-            }
+            songData.songImage = songPicture;
+          }
+        },
+        onError: function (error) {
+          console.log(error);
+        },
+      });
 
-            jsMediaTags.read(song, {
-                onSuccess: function (tag) {
-                    console.log(tag)
-                    songData.name = tag.tags.title;
-                    songData.artist = tag.tags.artist;
-                    songData.duration = (tag.tags.TLEN.data / 60000);
-                },
-                onError: function (error) {
-                    console.log(error)
-                }
-            })
+      return songData;
+    });
+    setPlaylistData({
+      ...playlistData,
+      tracks: songFiles,
+    });
+  }
 
-            return songData
-        })
-        setPlaylistData({
-            ...playlistData,
-            songs: songFiles
-        })
-    }
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        if (playlistData.coverImage && playlistData.playlistName && playlistData.songs.length) {
-            console.log(playlistData)
+  function handleLocalStorage() {
+    if (localStorage.getItem("Local Music")) {
+      const localMusic = JSON.parse(localStorage.getItem("Local Music"));
+      console.log(localMusic);
+      for (let i = 0; i < localMusic.length; i++) {
+        if (localMusic[i].playlistName === playlistData.playlistName) {
+          playlistData.playlistName += "(1)";
+          break;
         }
+      }
+      localMusic.push(playlistData);
+      localStorage.setItem(`Local Music`, JSON.stringify(localMusic));
+    } else {
+      localStorage.setItem(`Local Music`, JSON.stringify([playlistData]));
     }
+  }
 
-    return (
-        <form className="modal fade" id="file-upload" data-backdrop="static" aria-labelledby="file-upload-modal" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">Upload Local Music</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                        <div className="mb-3">
-                            <span id="inputGroup-sizing-default">Playlist Cover Image</span>
-                            <input type="file" className="form-control" required aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" onChange={handlePLaylistCoverChange} accept='image/*' />
-                        </div>
-                        <div className=" mb-3">
-                            <span id="inputGroup-sizing-default">Playlist Name</span>
-                            <input type="text" className="form-control" required aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value={playlistData.name} onChange={handlePlaylistChangeName} />
-                        </div>
-                        <div className="mb-3">
-                            <span id="inputGroup-sizing-default">Songs</span>
-                            <input className="form-control" type="file" required id="formFileMultiple" accept="audio/*" multiple onChange={handleFileUpload} />
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" form='file-upload' className="btn btn-primary" onClick={handleSubmit}>Submit</button>
-                    </div>
-                </div>
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (
+      playlistData.coverImage &&
+      playlistData.playlistName &&
+      playlistData.tracks.length
+    ) {
+      handleLocalStorage();
+    }
+  }
+
+  return (
+    <form
+      className="modal fade"
+      id="file-upload"
+      data-backdrop="static"
+      aria-labelledby="file-upload-modal"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Upload Local Music
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <div className="mb-3">
+              <span id="inputGroup-sizing-default">Playlist Cover Image</span>
+              <input
+                type="file"
+                className="form-control"
+                required
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-default"
+                onChange={handlePLaylistCoverChange}
+                accept="image/*"
+              />
             </div>
-        </form>
-    )
-}
+            <div className=" mb-3">
+              <span id="inputGroup-sizing-default">Playlist Name</span>
+              <input
+                type="text"
+                className="form-control"
+                required
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-default"
+                value={playlistData.name}
+                onChange={handlePlaylistChangeName}
+              />
+            </div>
+            <div className="mb-3">
+              <span id="inputGroup-sizing-default">Songs</span>
+              <input
+                className="form-control"
+                type="file"
+                required
+                id="formFileMultiple"
+                accept="audio/*"
+                multiple
+                onChange={handleFileUpload}
+              />
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button
+              type="submit"
+              form="file-upload"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
 
-export default FileUpload
+export default FileUpload;
