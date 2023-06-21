078ab741d29c9e1b9f7726312a6fa4bd026d4854
Jordan Dinis <jtyler9999@gmail.com>
2023-06-21 13:46:54 -0700

    feat(functionality): added return button for playlist view in mobile UI

--- src/components/Main-window.jsx
+++ src/components/Main-window.jsx
@@ -9,7 +9,10 @@ import PlaylistContainer from "./PlaylistContainer";
 import SettingsBar from "./SettingsBar";
 import CurrentSong from "./CurrentSong";
 import FileUpload from "./FileUpload";
+
+import returnImg from "../assets/return.svg"
 import "./main.css";
+import Container from "./Container.jsx";
 
 function Main() {
   const { userPlaylistSpotify } = useContext(Context)
@@ -35,7 +38,7 @@ function Main() {
       <div className="row h-100">
 
         {/* left column */}
-        <div className= {`col-12 col-md-6 ${libraryView ? "":"d-none"}`}>
+        <div className= {`col-12 col-md-6 ${libraryView ? "":"d-none d-md-block"}`}>
           {/* Nav/search bar */}
           <div className="col-12 ns-bar text-center">
             <Nav />
@@ -62,9 +65,14 @@ function Main() {
         </div>
 
         {/* right column */}
-        <div className={`col-12 col-md-6 ${!libraryView ? "":"d-none"}`}>
-          <div className="col-12 cur-text">
-            <h3>{library.length > 0 && library[playlistIndex].name}</h3>
+        <div className={`col-12 col-md-6 ${!libraryView ? "":"d-none d-md-block"}`}>
+          <div className="col-12 d-flex">
+            <button className="col-3 d-md-none" onClick={()=>setLibraryView(true)}>
+              <img src={returnImg} alt = "Return arrow"></img>
+            </button>
+            <div className="col-9 col-md-12 cur-text">
+              <h3>{library.length > 0 && library[playlistIndex].name}</h3>
+            </div>
           </div>
           {/* Current playlist */}
           <div className="col-12 cur-list">
