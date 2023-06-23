d37c9eb9368a0d37da309a8db2e088bdef5930a9
Traven Gonzales <travengonzales@gmail.com>
2023-06-23 16:13:22 -0700

    chore(structure): Removed hooks folder and content from project due to lack of use

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import Login from "./components/Login"
 import CurrentSongOffCanvas from "./components/CurrentSongOffCanvas"
 import Main from "./components/Main-window"
 import { Context } from "./contexts/Context"
+
 import "./App.css"
 
 function App() {
   const { accessToken } = useContext(Context);
 
   return <>
-      {accessToken ? <Main /> : <Login />}
-      <CurrentSongOffCanvas/>
-    </>;
+    {accessToken ? <Main /> : <Login />}
+    <CurrentSongOffCanvas />
+  </>;
 }
 
 export default App;
