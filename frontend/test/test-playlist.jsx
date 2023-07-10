c28cd185dbe480fda7ee65ac8675e3b5c053304d
Traven Gonzales <travengonzales@gmail.com>
2023-07-10 16:12:16 -0700

    test(data): Added playlist test data

---
+++ frontend/test/test-playlist.jsx
@@ -0,0 +1,9 @@
+import testSong1 from "../assets/02 - Guillotine.mp3"
+import testSong2 from "../assets/03 - Spread Eagle Cross the Block.mp3"
+import testSong3 from "../assets/05 - Takyon (Death Yon).mp3"
+
+export default [
+    testSong1,
+    testSong2,
+    testSong3
+]
