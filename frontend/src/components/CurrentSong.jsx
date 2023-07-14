a1f8c687a92499a066fbe8f211f2695865ba7ab2
Traven Gonzales <travengonzales@gmail.com>
2023-07-14 16:00:27 -0700

    fix(bug): Fixed crossorigin request blocked bug for playback of files from backend

--- frontend/src/components/CurrentSong.jsx
+++ frontend/src/components/CurrentSong.jsx
@@ -60,8 +60,8 @@ function CurrentSong() {
             </div>
 
             <div className="current-song-right-side col-4 col-md-6 h-100">
-                <div className="song-bar justify-content-between d-none d-md-flex mb-2">
-                    <span className="pe-2">
+                <div className="song-bar justify-content-between d-none d-md-flex mb-2 align-items-center">
+                    <span className="pe-2 fs-6">
                         {convertToTimestamp(localPlayback.playedSeconds)}
                     </span>
                     <input
@@ -76,7 +76,7 @@ function CurrentSong() {
                         id="range"
                         className="custom-range"
                     />
-                    <span className="ps-2">
+                    <span className="ps-2 fs-6">
                         {convertToTimestamp(duration)}
                     </span>
                 </div>
